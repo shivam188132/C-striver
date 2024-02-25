2b9161d9347cd50deb139e60d1171e548e3d01d7
Shivam <Shivamkumar188132@gmail.com>
2024-02-25 20:34:23 +0530

    'second'

--- Step1-1.cpp
+++ Step1-1.cpp
@@ -22,6 +22,7 @@ using namespace std;
 // for adding new line we use endl or "\n" character
 // for doing multiple line comments press Shift + Alt + A
 
+// for printing two variable x and y
 
 /* int main(){
 
@@ -32,17 +33,73 @@ using namespace std;
 } */
 
 
-int main() {
+// for including all the libraries that cpp has
+// we use 
+// #include <bits/stdc++.h>
 
-    // int x
-    // int x = 10;
-    // long
-    long x1;
-    // cin >> x;
-    // long long
-    // long long x = 15000000;
-    cin >> x1 ;
-    cout << "Value of x1: " << x1 << endl;
+
+// int main() {
+
+//     // int x
+//     // int has a value b/w -2,147,483,648 to 2,147,483,647 and takes a 4 Bytes as space 
+//     // int x = 10;
+//     // beyond that range we use long
+  
+//     long x1;
+//     // cin >> x;
+//     // long long
+//     // long long x = 15000000;
+//     cin >> x1 ;
+//     cout << "Value of x1: " << x1 << endl;
+//     return 0;
+// }
+
+
+// float data type
+
+// int main() {
+
+//     // floating data types
+//     float x =5.6;
+//     float y ;
+//     // double is a expansion of float just for a large value
+//     double z = 34.44;
+//     cin >> y ;
+//     cout <<"Value of y is: " <<  y << endl;
+//     cout <<"Value of z is: " <<  z << endl;
+
+//     return 0;
+// }
+
+
+// string data type
+
+// int main(){
+
+//     string s;
+//     // only print Hello if input hello Striver was given to print both
+//     string s2;
+
+//     cin >> s>> s2;
+//     cout << s << " "  << s2;
+//     return 0;
+
+
+// }
+
+int main(){
+
+    // in case for printing togethger in  a line insted of s,s2 we use getline
+    string str;
+    getline(cin, str);
+    cout << str;
     return 0;
+
+
 }
+
+
+
+
+
 // ctrl+ shift + b to run
