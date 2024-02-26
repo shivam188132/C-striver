c9e188f560249f94e99e43194ca5321f453cd090
Shivam <Shivamkumar188132@gmail.com>
2024-02-26 15:13:08 +0530

    'coading_Ng'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -32,6 +32,21 @@ int main() {
 }
 // -----------------------------------------------------------------------------
 
+// Problem statement
+// Data type refers to the type of value a variable has and the way the computer interprets it.
+
+
+
+// Each data type has a different size. You’ve studied 5 different data types and the sizes of the data types:
+
+// Integer: 4 bytes
+// Long: 8 bytes
+// Float: 4 bytes
+// Double: 8 bytes
+// Character: 1 byte
+
+
+// ----------------------------------------------------------------------------------
 int dataTypes(string type) {
 	cin >> type;
     // Write your code here
