49737fab5d499654d4fad8abed3b5e877acfddf0
Shivam <Shivamkumar188132@gmail.com>
2024-02-27 12:56:52 +0530

    'thee'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -8,7 +8,7 @@
 // -1, if the character is not an alphabet.
 
 // -------------------------------------------------
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -97,4 +97,45 @@ string compareIfElse(int a, int b) {
 	}
 	
 	else { return "equal"; }
+}
+
+// Problem statement
+// Programming languages have some conditional / decision-making statements that execute when some specific condition is fulfilled.
+// Switch-case is one of the ways to implement them.
+// In a menu-driven program, the user is given a set of choices of things to do (the menu) and then is asked to select a menu item.
+// There are 2 choices in the menu:
+// Choice 1 is to find the area of a circle having radius 'r'.
+// Choice 2 is to find the area of a rectangle having dimensions 'l' and 'b'.
+// You are given the choice 'ch' and an array 'a'.
+// If ‘ch’ is 1, ‘a’ contains a single number ‘r’. If ‘ch’ is 2, ‘a’ contains 2 numbers, ‘l’ and ‘b’.
+// Consider the choice and print the appropriate area.
+// Example :
+// Input: ‘ch’ = 2 and ‘a’ = [3, 2]
+// Output: area = 6
+// Explanation: Since the choice ‘ch’ is 2, we have to print the area of the rectangle having ‘l’ = 3 and ‘b’ = 2, which is 6.
+
+
+// ------------------------------------------------------------------------------------------------------------
+
+
+
+double areaSwitchCase(int ch, vector<double> a) {
+	// Write your code here
+	switch (ch ) {
+
+		case 1:
+		// area of circle
+			return M_PI *a[0]*a[0];
+			break;
+
+		case 2:
+
+			return a[0]*a[1];
+			break;
+
+		default:
+		
+			return 0;
+	}
+	
 }
