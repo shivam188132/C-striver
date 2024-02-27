33cc038255b73f9a3651d954899c513128f6e261
Shivam <Shivamkumar188132@gmail.com>
2024-02-27 20:47:46 +0530

    '56'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -207,3 +207,57 @@ int main()
 
     return 0;
 }
+
+
+/*
+Problem statement
+Write a program to input an integer 'n' and print the sum of all its even digits and the sum of all its odd digits separately.
+
+
+
+Digits mean numbers, not places! That is, if the given integer is "132456", even digits are 2, 4, and 6, and odd digits are 1, 3, and 5.
+
+Constraints
+0<= 'n' <=10000
+
+
+Example :
+Input: 'n' = 132456
+
+Output: 12 9
+
+Explanation:
+The sum of even digits = 2 + 4 + 6 = 12
+The sum of odd digits = 1 + 3 + 5 = 9
+Constraints
+0<= 'n' <=10000
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int i;
+	cin >> i;
+	int sum_even =0, sum_odd =0;
+
+	// looping through each digit
+	while(i>0){
+		// extract the last digit
+		// suppose 143 then 143%10 is 3
+		int digit = i%10;
+		if (digit % 2 ==0){
+			sum_even +=digit;
+		}
+		else {
+			sum_odd +=digit;
+		}
+		// removing last digit 
+		i /=10; 
+
+	}
+	cout << sum_even << " "<< sum_odd;
+
+	return 0;
+	
+}
