03b91fc0f6bf8763e18f171f33eb843577d6cd63
Shivam <Shivamkumar188132@gmail.com>
2024-02-27 11:59:30 +0530

    '3rd_question_ninja'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -61,3 +61,40 @@ int dataTypes(string type) {
     }
     return 0;
 }
+
+
+// Problem statement
+// Programming languages have some conditional / decision-making statements that execute when some specific condition is fulfilled.
+
+
+
+// If-else is one of the ways to implement them.
+
+
+
+// You are given two numbers 'a' and 'b'.
+
+
+
+// Compare the numbers and print the relation.
+
+
+
+// Print “smaller”, “greater” or “equal” when ‘a’ is smaller than ‘b’, greater than ‘b’, or equal to ‘b’ respectively.
+
+
+// ---------------------------------------------------------------------------------------------------------
+
+
+string compareIfElse(int a, int b) {
+	// Write your code here
+	if (a < b) {
+		return "smaller";
+	}
+
+	else if (a >b ) {
+		return "greater";
+	}
+	
+	else { return "equal"; }
+}
