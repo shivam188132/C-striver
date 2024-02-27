540b28745b308b5ae372c7dd9196baeaf51d5f1b
Shivam <Shivamkumar188132@gmail.com>
2024-02-27 13:43:36 +0530

    'cn6'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -139,3 +139,71 @@ double areaSwitchCase(int ch, vector<double> a) {
 	}
 	
 }
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+// Function to calculate the nth Fibonacci number
+
+
+
+// Problem statement
+// The n-th term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+
+//     F(n) = F(n - 1) + F(n - 2), 
+//     Where, F(1) = 1, F(2) = 1
+
+
+// Provided 'n' you have to find out the n-th Fibonacci Number. Handle edges cases like when 'n' = 1 or 'n' = 2 by using conditionals like if else and return what's expected.
+
+// "Indexing is start from 1"
+
+
+// Example :
+// Input: 6
+
+// Output: 8
+
+// Explanation: The number is ‘6’ so we have to find the “6th” Fibonacci number.
+// So by using the given formula of the Fibonacci series, we get the series:    
+// [ 1, 1, 2, 3, 5, 8, 13, 21]
+// So the “6th” element is “8” hence we get the output.
+
+
+
+// ------------------------------------------------------------------------------------
+
+int fibonacci(int n) {
+    // Base cases: F(1) = 1 and F(2) = 1
+    if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        // Recursive case: F(n) = F(n-1) + F(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+
+
+int main()
+{
+        
+    int n;
+
+    // Get input for the nth term
+    
+    cin >> n;
+
+    // Call the fibonacci function to find the nth term
+    int nth_term = fibonacci(n);
+
+    // Print the result
+    cout << nth_term << endl;
+
+    return 0;
+}
