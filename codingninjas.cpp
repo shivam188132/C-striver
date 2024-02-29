8730cacabab50c86c23adbac93785dccf2aff725
Shivam <Shivamkumar188132@gmail.com>
2024-02-29 08:37:26 +0530

    '29th_2'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -325,3 +325,66 @@ void nForest(int n) {
 	}
 }
 
+/*
+Problem statement
+Sam is making a Triangular painting for a maths project.
+
+An N-dimensional Triangle is represented by the lower triangle of the pattern filled with integers starting from 1.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+1
+1 2 
+1 2 3
+
+*/
+
+void nTriangle(int n) {
+	// Write your code here
+	int i,j;
+	for (i=0; i<n; i++){
+		for (j=0; j<=i; j++){
+			cout << j+1<< " ";
+		}
+		cout << endl;
+	}
+}
+
+
+/*
+Problem statement
+Sam is making a Triangular painting for a maths project.
+
+An N-dimensional Triangle is represented by the lower triangle of the pattern filled with integers representing the row number.
+
+For every value of ‘N’, help sam to print the corresponding Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+1
+2 2 
+3 3 3
+
+*/
+
+void triangle(int n) {
+	// Write your code here
+	int i ,j;
+	for (i=0; i<n ; i++){
+		for (j=0 ; j<=i ; j++ ){
+			cout << i+1<< " ";
+		}
+		cout << endl;
+	}
+}
+
+
+
+
+
--- step 1-2 day5.cpp
+++ step 1-2 day5.cpp
@@ -39,7 +39,7 @@ int main(){
 
 */
 
-void nForest(int n) {
+void nForest2(int n) {
 	// Write your code here.
 	int i ,j;
 	for (i=0; i<n; i++){
@@ -53,5 +53,56 @@ void nForest(int n) {
 int main(){
     int n;
     cin >> n;
-    nForest(n);
+    nForest2(n);
+}
+
+
+/*
+
+1
+1 2 
+1 2 3
+
+*/
+
+void nTriangle(int n) {
+	// Write your code here
+	int i,j;
+	for (i=0; i<n; i++){
+		for (j=0; j<=i; j++){
+			cout << j+1<< " ";
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+    int n;
+    cin >> n;
+    nTriangle(n);
+}
+
+
+/*
+1
+2 2 
+3 3 3
+
+*/
+
+void triangle2(int n) {
+	// Write your code here
+	int i ,j;
+	for (i=0; i<n ; i++){
+		for (j=0 ; j<=i ; j++ ){
+			cout << i+1<< " ";
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+    int n;
+    cin >> n;
+    triangle2(n);
 }
