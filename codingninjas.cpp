c078bb2c5c176b0ad21df67437edac67ad653bcb
Shivam <Shivamkumar188132@gmail.com>
2024-02-29 08:21:02 +0530

    '29th_2'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -293,3 +293,35 @@ void nForest(int n) {
 		cout << endl;
 	}
 }
+
+/*
+
+Problem statement
+Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+An N/2-dimensional forest is represented by the lower triangle of the pattern filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N/2-dimensional forest.
+
+Example:
+Input:  ‘N’ = 3
+
+Output: 
+* 
+* *
+* * *
+
+*/
+
+
+void nForest(int n) {
+	// Write your code here.
+	int i ,j;
+	for (i=0; i<n; i++){
+		for (j=0;j<=i; j++){
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
+
--- step 1-2 day5.cpp
+++ step 1-2 day5.cpp
@@ -29,3 +29,29 @@ int main(){
     cin >> n;
     nForest(n);
 }
+
+
+/*
+
+* 
+* * 
+* * * 
+
+*/
+
+void nForest(int n) {
+	// Write your code here.
+	int i ,j;
+	for (i=0; i<n; i++){
+		for (j=0;j<=i; j++){
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+    int n;
+    cin >> n;
+    nForest(n);
+}
