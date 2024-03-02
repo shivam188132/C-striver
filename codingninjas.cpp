7400caa0353bb1de45d31fa25e8e9fa45b583c3d
Shivam <Shivamkumar188132@gmail.com>
2024-03-02 13:27:55 +0530

    '7thq'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -446,3 +446,32 @@ void nStarTriangle(int n) {
 }
 
 
+/* 
+
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+***** 
+
+*/
+
+void nStarTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl; // Move to the next line after each row
+    }
+}
