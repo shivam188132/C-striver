7a74f1711d6be73083ea50136dc4bb2df52882b3
Shivam <Shivamkumar188132@gmail.com>
2024-03-03 21:16:11 +0530

    '3rdmar'

--- step 1-2 day5.cpp
+++ step 1-2 day5.cpp
@@ -1,131 +1,131 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// /*
+/*
 
-// * * * 
-// * * * 
-// * * * 
+* * * 
+* * * 
+* * * 
 
-// */
+*/
 
 
-// void nForest(int n) {
-// 	// Write your code here.
-// 	// int n;
+void nForest(int n) {
+	// Write your code here.
+	// int n;
 	
-// 	int i,j;
+	int i,j;
 	
-// 	for(i=0 ;i<n; i++){
-// 		for (j=0 ;j<n ;j++){
-// 			cout << '*' << " ";
-// 		}
-// 		cout << endl;
-// 	}
-// }
+	for(i=0 ;i<n; i++){
+		for (j=0 ;j<n ;j++){
+			cout << '*' << " ";
+		}
+		cout << endl;
+	}
+}
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     nForest(n);
-// }
-
-
-// /*
-
-// * 
-// * * 
-// * * * 
-
-// */
-
-// void nForest2(int n) {
-// 	// Write your code here.
-// 	int i ,j;
-// 	for (i=0; i<n; i++){
-// 		for (j=0;j<=i; j++){
-// 			cout << "* ";
-// 		}
-// 		cout << endl;
-// 	}
-// }
-
-// int main(){
-//     int n;
-//     cin >> n;
-//     nForest2(n);
-// }
+int main(){
+    int n;
+    cin >> n;
+    nForest(n);
+}
 
 
-// /*
+/*
 
-// 1
-// 1 2 
-// 1 2 3
+* 
+* * 
+* * * 
 
-// */
+*/
 
-// void nTriangle(int n) {
-// 	// Write your code here
-// 	int i,j;
-// 	for (i=0; i<n; i++){
-// 		for (j=0; j<=i; j++){
-// 			cout << j+1<< " ";
-// 		}
-// 		cout << endl;
-// 	}
-// }
+void nForest2(int n) {
+	// Write your code here.
+	int i ,j;
+	for (i=0; i<n; i++){
+		for (j=0;j<=i; j++){
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     nTriangle(n);
-// }
+int main(){
+    int n;
+    cin >> n;
+    nForest2(n);
+}
 
 
-// /*
-// 1
-// 2 2 
-// 3 3 3
+/*
 
-// */
+1
+1 2 
+1 2 3
 
-// void triangle2(int n) {
-// 	// Write your code here
-// 	int i ,j;
-// 	for (i=0; i<n ; i++){
-// 		for (j=0 ; j<=i ; j++ ){
-// 			cout << i+1<< " ";
-// 		}
-// 		cout << endl;
-// 	}
-// }
+*/
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     triangle2(n);
-// }
+void nTriangle(int n) {
+	// Write your code here
+	int i,j;
+	for (i=0; i<n; i++){
+		for (j=0; j<=i; j++){
+			cout << j+1<< " ";
+		}
+		cout << endl;
+	}
+}
 
-// /*
+int main(){
+    int n;
+    cin >> n;
+    nTriangle(n);
+}
 
-// * * *
-// * *
-// *
 
-// */
+/*
+1
+2 2 
+3 3 3
 
-// void seeding(int n) {
-// 	// Write your code here.
-// 	int i,j;
-// 	 for (int i = 0; i < n; i++) {
-//         // Adjust the inner loop to print '*' until n - i
-//         for (int j = 0; j < n - i; j++) {
-//           cout << "* "; // Print '*'
-//         }
-//         cout << endl; // Move to the next line after each row
-//          }
-// }
+*/
+
+void triangle2(int n) {
+	// Write your code here
+	int i ,j;
+	for (i=0; i<n ; i++){
+		for (j=0 ; j<=i ; j++ ){
+			cout << i+1<< " ";
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+    int n;
+    cin >> n;
+    triangle2(n);
+}
+
+/*
+
+* * *
+* *
+*
+
+*/
+
+void seeding(int n) {
+	// Write your code here.
+	int i,j;
+	 for (int i = 0; i < n; i++) {
+        // Adjust the inner loop to print '*' until n - i
+        for (int j = 0; j < n - i; j++) {
+          cout << "* "; // Print '*'
+        }
+        cout << endl; // Move to the next line after each row
+         }
+}
 
 
 /* 
@@ -159,6 +159,149 @@ void nStarTriangle(int n) {
 }
 
 
+/* 
+
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the Reverse N-Star Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*****
+ ***
+  *
+  
+ */
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    nStarTriangle(n);
+}
+
+
+void nStarTriangle(int n) {
+    // Write your code here.
+	for (int i=0;i<n;i++){
+		// space
+		for(int j=0;j<i;j++){
+			cout << " ";
+		}
+		// star
+		for (int k=0;k < 2*n- (2*i+1);k++){
+			cout << "*";
+		}
+		cout << endl;
+	}
+    
+}
+
+/* 
+
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Diamond.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+*****
+ ***
+  *
+
+ */
+
+
+void nStarDiamond(int n) {
+    // Write your code here.
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl; // Move to the next line after each row
+    }
+
+    for (int i=0;i<n;i++){
+		// space
+		for(int j=0;j<i;j++){
+			cout << " ";
+		}
+		// star
+		for (int k=0;k < 2*n- (2*i+1);k++){
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+
+
+void nStarTriangle(int n) {
+    // Write your code here.
+    for (int i=1;i<2*n-1;i++){
+      int stars =i;
+      if (i>n) stars =2*n - i ;
+      for (int j=1; j<= stars; j++){
+        cout << "*";
+      }
+      cout << endl;
+    }
+}
+
+// m2
+void nStarTriangle(int n) {
+    // Upper triangle
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+    // Lower triangle
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -167,5 +310,5 @@ void nStarTriangle(int n) {
 int main(){
     int n;
     cin >> n;
-    nStarTriangle(n);
+    // nStarTriangle(n);
 }
