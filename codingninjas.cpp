d92ec7c575dab2979b08818bdcfedd2abf72afea
Shivam <Shivamkumar188132@gmail.com>
2024-03-06 23:05:46 +0530

    '6thcome'

--- codingninjas.cpp
+++ codingninjas.cpp
@@ -674,3 +674,48 @@ void alphaHill(int n) {
     }
     
 }
+
+/*
+
+Problem statement
+Sam is curious about symmetric patterns, so he decided to create one.
+
+For every value of ‘N’, return the symmetry as shown in the example.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * * * * * 
+* *     * * 
+*         * 
+*         * 
+* *     * * 
+* * * * * * 
+
+*/
+
+void symmetry(int n) {
+    for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n-i;j++){
+        cout<<"* ";}
+    for(int j=0;j<(2*i);j++){
+        cout<<"  ";}
+    for(int j=0;j<n-i;j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+for(int i=n-1;i>=0;i--)
+{
+    for(int j=0;j<n-i;j++){
+        cout<<"* ";}
+    for(int j=0;j<(2*i);j++){
+        cout<<"  ";}
+    for(int j=0;j<n-i;j++){
+        cout<<"* ";}
+        cout<<endl;
+}
+
+}
--- test.cpp
+++ test.cpp
@@ -2,20 +2,51 @@
 #include <string>
 using namespace std;
 
-void nLetterTriangle(int n) {
+void symmetry(int n) {
     // Write your code here.
-    for (int i =1; i<=n ;i++){
-        for (char ch='A';ch <'A'+ (n-i); ch++)
-        {cout << ch<< " ";}
+    int space =0;
+    for (int i =0; i<n; i++){
+        // stars
+        for (int j=1; j<=n-i; j++){
+            cout << "*";
+        }
+        // space
+        for (int j=0;j<space;j++){
+            cout << " ";
+        }
+        //stars
+        for (int j=1;j<=n-i;j++){
+            cout << "*";
 
+        }
+        space +=2;
+        cout << endl;
     }
+
+
+    
+    int space2 = 2*n -2;
+    for (int i=0; i<n ; i++){
+        for (int j=0; j<=i; j++){
+            cout<< "*";
+      }
+    //   space
+    for (int j=0; j<space;j++){
+        cout << " ";
+        //stars
+    for(int j=0; j<=i;j++){
+        cout << "*";
+    }
+    space2 -=2;
     cout << endl;
+    }
+    }
+
 
 
 }
-
 int main(){
     int n;
     cin >> n;
-     nLetterTriangle(n);
+    symmetry(n);
 }
