747df1fefc7d14cca97fe4b247cf25a0430dc473
Shivam <Shivamkumar188132@gmail.com>
2024-03-06 11:46:25 +0530

    '6thmar3d'

--- step 1-2 day5.cpp
+++ step 1-2 day5.cpp
@@ -340,7 +340,26 @@ void nNumberTriangle(int n) {
 }
 
 
+/*
 
+Output: 
+
+A
+A B
+A B C
+
+*/
+
+void nLetterTriangle(int n) {
+    // Write your code here.
+    for (int i=0; i<n ;i++){
+
+        for (char ch ='A'; ch <='A'+i; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
 
 
 
