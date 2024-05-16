88d5852125213407c7797ace65792452fbbeb4ff
Shivam <Shivamkumar188132@gmail.com>
2024-05-16 10:22:27 +0530

    't;t

--- 23.py
+++ 23.py
@@ -1,5 +1,12 @@
+import pandas as pd
 
-for i in range(5):
-  print(i, end="\n")
+# Sample DataFrame with a string date column
+data = {'date': ['2024-05-14', '2024-05-15', '2024-05-16']}
+df = pd.DataFrame(data)
+print(type(df['date'] ))
+# Convert string date column to datetime
+df['date'] = pd.to_datetime(df['date'])
 
-
+# Now the 'date' column is in datetime format
+print(df)
+print(type(df['date'] ))
