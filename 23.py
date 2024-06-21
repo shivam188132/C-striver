6dfcee818da439ace738af07ae95cc8f8c29969a
Shivam <Shivamkumar188132@gmail.com>
2024-06-21 20:06:01 +0530

    '21-06'

--- 23.py
+++ 23.py
@@ -1,4 +1,24 @@
-magic = [1,2,3]
-a = len(magic)
-print(type(a))
-print("hye")
+# def maxSubArray(nums):
+#         maxSum = float('-inf')
+#         currentSum = 0
+        
+#         for num in nums:
+#             currentSum += num
+#             print(currentSum)
+            
+#             if currentSum > maxSum:
+#                 maxSum = currentSum
+            
+#             if currentSum < 0:
+#                 currentSum = 0
+        
+#         return maxSum
+
+# maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+freq = 3*[0]
+for x in [2,0,2,1,1,0]:
+    print(x)
+    freq[x]+=1
+print(freq)
+
+    
