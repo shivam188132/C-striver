0b487fe001faf668a5eb9ec630ecdfd39ce30826
Shivam <Shivamkumar188132@gmail.com>
2024-07-17 21:44:12 +0530

    17-07-2-2024

--- 23.py
+++ 23.py
@@ -15,10 +15,13 @@
 #         return maxSum
 
 # maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
-freq = 3*[0]
-for x in [2,0,2,1,1,0]:
-    print(x)
-    freq[x]+=1
-print(freq)
+# freq = 3*[0]
+# for x in [2,0,2,1,1,0]:
+#     print(x)
+#     freq[x]+=1
 
+# print(freq)
+
+x = [7,1,5,3,6,4]
+print(len(x))
     
---
+++ testing_leetcode.py
@@ -0,0 +1,4 @@
+def a(matrix):
+    return len(matrix)
+
+a([[1,2,3],[4,5,6],[7,8,9]])
